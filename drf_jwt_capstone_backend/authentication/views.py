c8527cf0fd05bf173296660303a68d37da3f5839
Midnight <arreiskurai@yahoo.com>
2021-11-11 21:50:14 -0800

    Added ability to edit user for profile

--- drf_jwt_capstone_backend/authentication/views.py
+++ drf_jwt_capstone_backend/authentication/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 
 User = get_user_model()
@@ -15,10 +16,18 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegistrationSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
 def user(request, id):
     if request.method == 'GET':
         user = User.objects.get(id = id)
         serializer = RegistrationSerializer(user)
         return Response(serializer.data)
+
+    if request.method == 'PUT':
+        user = User.objects.get(id=id)
+        serializer = RegistrationSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
